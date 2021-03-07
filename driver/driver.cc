be61a2b07134ee85eb8157dac88a779c2f20f78c
uslsteen <shurygin0171@gmail.com>
2021-03-07 04:42:10 +0300

    added driver from paracl

---
+++ driver/driver.cc
@@ -0,0 +1,128 @@
+#include "driver.hh"
+
+
+//! Constructor for class Driver
+//! \param name_of_file - the name of the file from which our program is read
+yy::Driver::Driver(const char *name_of_file) : name_of_file_(name_of_file)
+{
+  std::string tmp_str;
+
+  in_file.open(name_of_file);
+  std::ifstream tmp(name_of_file);
+
+  if (tmp.is_open())
+  {
+    while (tmp)
+    {
+      std::getline(tmp, tmp_str);
+      lines_of_prog.push_back(tmp_str);
+    }
+  }
+  else
+  {
+    std::string what = "File '" + name_of_file_ + "' does not exist";
+    throw std::runtime_error{what};
+  }
+
+  plex_ = new OurFlexLexer;
+  plex_->switch_streams(in_file, std::cout);
+}
+
+//! Functuion for calling bison yy::parser:parse()
+//! \return bool in
+bool yy::Driver::parse()
+{
+  yy::parser parser_(this);
+
+  bool res = !parser_.parse();
+  return res;
+}
+
+//! The lexical analyzer function, yylex, recognizes tokens from the input stream and returns them to the parser.
+//! \param yylval
+//! \param yylloc
+//! \return token type
+yy::parser::token_type yy::Driver::yylex(yy::parser::semantic_type *yylval, parser::location_type *yylloc)
+{
+  yy::parser::token_type tkn_type = static_cast<yy::parser::token_type>(plex_->yylex());
+
+  switch (tkn_type)
+  {
+  case yy::parser::token_type::INT: {
+    yylval->emplace<int>(std::stoi(plex_->YYText()));
+    break;
+  }
+
+  case yy::parser::token_type::NAME: {
+    yylval->emplace<std::string>(std::string{plex_->YYText()});
+    break;
+  }
+
+  case yy::parser::token_type::ERR: {
+    std::cerr << "UNKNOWN TOKEN" << std::endl;
+  }
+
+  default:
+    break;
+  }
+  *yylloc = plex_->get_cur_location();
+  return tkn_type;
+}
+
+void yy::Driver::insert(int jnction1, int jnction2, float resistor, float voltage)
+{
+    std::cout << "Hello, I'm insertion\n";
+    return;
+}
+
+//!  Function for processing syntax error during parsing
+//! \param ctx - the context that is created when an error is found
+void yy::Driver::report_syntax_error(const parser::context &ctx)
+{
+  yy::location loc = ctx.location();
+
+  std::cerr << "syntax error in ";
+  std::cerr << "line: " << loc.begin.line;
+  std::cerr << ", column: " << loc.begin.column << std::endl;
+
+  // Report the tokens expected at this point.
+  parser::symbol_kind_type expectd_tokns[NUM_OF_TOKENS];
+  size_t num_of_expectd_tokns = ctx.expected_tokens(expectd_tokns, NUM_OF_TOKENS);
+
+  std::cerr << "expected:";
+
+  for (size_t i = 0; i < num_of_expectd_tokns; ++i)
+  {
+    if (i != 0)
+      std::cerr << " or ";
+
+    std::cerr << " <" << parser::symbol_name(expectd_tokns[i]) << "> ";
+  }
+
+  std::cerr << std::endl;
+
+  // Report the unexpected token.
+  parser::symbol_kind_type lookahead = ctx.token();
+
+  std::cerr << "before: "
+            << "<" << parser::symbol_name(lookahead) << ">" << std::endl;
+  std::cerr << loc.begin.line << "   |   " << lines_of_prog[loc.begin.line - 1] << std::endl;
+  std::cerr << "    |   ";
+
+  for (int i = 0; i < loc.end.column - 1; ++i)
+  {
+    if (i == (ctx.lookahead().location.begin.column - 1))
+      std::cerr << "^";
+
+    else
+      std::cerr << "~";
+  }
+
+  std::cerr << std::endl;
+}
+
+//! Destructor for class Driver
+yy::Driver::~Driver()
+{
+  delete plex_;
+}
---
+++ driver/driver.hh
@@ -0,0 +1,57 @@
+
+#ifndef PARACL_DRIVER_HH
+#define PARACL_DRIVER_HH
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "../parser/parser.hh"
+
+#ifndef yyFlexLexer
+#include <FlexLexer.h>
+#endif /* yyFlexLexer */
+
+#ifndef yyFlexLexer
+//#include "../FB_BLD/lex.yy.cc"
+#endif /* yyFlexLexer */
+
+enum
+{
+  NUM_OF_TOKENS = 30
+};
+
+namespace yy
+{
+class Driver final
+{
+private:
+  std::string name_of_file_;
+  std::ifstream in_file;
+
+  OurFlexLexer *plex_; // maybe this is good name
+  std::vector<std::string> lines_of_prog;
+
+public:
+  Driver(const char *name_of_file);
+
+  Driver(const Driver &drvr) = delete;
+
+  Driver &operator=(const Driver &) = delete;
+
+  bool parse();
+
+  void insert(int jnction1, int jnction2, float resistor, float voltage);
+
+  parser::token_type yylex(parser::semantic_type *yylval, parser::location_type *yylloc);
+
+  void report_syntax_error(const parser::context &ctx);
+
+  ~Driver();
+};
+
+} // namespace yy
+
+// namespace yy
+
+#endif // PARACL_DRIVER_HH
