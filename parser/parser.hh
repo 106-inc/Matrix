843bd87a2914aa25b4671f45be9a9e4f2f5e3ace
uslsteen <shurygin0171@gmail.com>
2021-03-07 04:43:27 +0300

    added parser from paracl

---
+++ parser/parser.hh
@@ -0,0 +1,42 @@
+#ifndef PARACL_PARSER_HH
+#define PARACL_PARSER_HH
+
+#include <iostream>
+
+#ifndef yyFlexLexer
+#include <FlexLexer.h>
+#endif /* yyFlexLexer */
+
+#include "../FB_BLD/compiler.tab.hh"
+
+enum RES_OF_PARSE
+{
+    SUCESS_PARSE, ERROR_IN_PARSE
+};
+
+class OurFlexLexer : public yyFlexLexer
+{
+private:
+  yy::location cur_location_{};
+
+  int last_num_of_line;
+
+public:
+  OurFlexLexer() = default;
+
+  OurFlexLexer(const OurFlexLexer &flx) = delete;
+  OurFlexLexer &operator=(const OurFlexLexer &) = delete;
+
+  yy::location get_cur_location();
+  int get_last_line_();
+
+  bool is_empty_line(const char *str);
+
+  void upd_cur_loc();
+
+  int yylex() override;
+
+  ~OurFlexLexer() = default;
+};
+
+#endif // PARACL_PARSER_HH
