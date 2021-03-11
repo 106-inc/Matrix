effe6648cc16024a1d9695a37d75a17366181c89
Tako-San <dxe13@mail.ru>
2021-03-11 19:03:04 +0300

    1) main.cc moved to lin_alg/main_la.cc 2) la.hh created 3) testd added

---
+++ lin_alg/la.hh
@@ -0,0 +1,49 @@
+#ifndef MATRIX_LA_HH
+#define MATRIX_LA_HH
+
+#include "matrix.hh"
+#include <vector>
+
+namespace LA
+{
+using namespace MX;
+
+class Sys : public Matrix<double>
+{
+  using Matrix<double>::rows_;
+
+public:
+  Sys(const Matrix &orig) : Matrix(orig)
+  {
+  }
+  Sys(size_t rows, size_t cols) : Matrix(rows, cols)
+  {
+  }
+  Sys(size_t rows, size_t cols, func action) : Matrix(rows, cols, action)
+  {
+  }
+  Sys(size_t rows, size_t cols, const initializer_list<double> &dat) : Matrix(rows, cols, dat)
+  {
+  }
+
+  std::vector<double> solve() const;
+};
+
+std::vector<double> Sys::solve() const
+{
+  Sys tmp{*this};
+
+  tmp.GaussFWD();
+  tmp.GaussBWD();
+
+  std::vector<double> res;
+  res.reserve(rows_);
+
+  for (size_t i = 0, col_idx = cols_ - 1; i < rows_; ++i)
+    res.push_back(tmp[i][col_idx] / tmp[i][i]);
+
+  return res;
+}
+} // namespace LA
+
+#endif // MATRIX_LA_HH
