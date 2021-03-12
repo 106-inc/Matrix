aa7297a3b52a865f0572bed22399b583a1a98bb0
uslsteen <shurygin0171@gmail.com>
2021-03-12 23:19:00 +0300

    added ctor for struct edge

--- circuits.hh
+++ circuits.hh
@@ -3,8 +3,7 @@
 
 #include <vector>
 
-#include "lin_alg/la.hh"
-#include "lin_alg/matrix.hh"
+//#include "lin_alg/la.hh"
 
 // TODO: найти где будут Edge
 
@@ -12,9 +11,15 @@ namespace CTS
 {
 struct Edge final // rtor, junc1, junc2, voltage
 {
-  size_t junc1_, junc_2;
+  size_t junc1, junc2;
 
-  double rtor_, eds_, cur_ /** May be deleted in future */;
+  double rtor, eds, cur /** May be deleted in future */;
+
+  Edge(size_t junc1_, size_t junc2_, double rtor_, double eds_) : junc1(junc1_),
+                                                                  junc2(junc2_),
+                                                                  rtor(rtor_),
+                                                                  eds(eds_)
+  {}
 };
 
 /**
@@ -24,19 +29,35 @@ class Circuit final
 {
 private:
   std::vector<Edge> edges_;
-  MX::Matrix<double> incidence_;
+ // MX::Matrix<int> incidence_;
 
 public:
 
   Circuit( const std::vector<Edge> &edges, size_t j_num );
 
-  
+  /*
+   * What here should locate:
+   * 1) Calculating matrix B
+   * 2) calculating currents
+   */
 
+
+  void curs_calc();
 
 private:
 
-  MX::Matrix<double> make_eds_matr();
-  MX::Matrix<double> make_res_matr();
+  enum class Color
+  {
+    WHITE = 0, GREY, BLACK
+  };
+
+  //MX::Matrix<double> make_eds_matr();
+ // MX::Matrix<double> make_res_matr();
+
+  //MX::Matrix<int> make_circ_matr();
+
+  void dfs( size_t nstart, size_t nactual, std::vector<Edge> &cyc_rout, std::vector<Color> &colors );
+  std::vector<Edge> dfs_start( size_t from );
 };
 } // namespace CTS
 
