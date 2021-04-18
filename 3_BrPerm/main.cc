9cae610bdb78af1a576cec442021593a9ea778c3
uslsteen <shurygin0171@gmail.com>
2021-04-19 01:13:49 +0300

    correct test main

--- 3_BrPerm/main.cc
+++ 3_BrPerm/main.cc
@@ -7,13 +7,36 @@ int main(void)
   size_t amount{};
   std::cin >> amount;
 
+  int tmp_elem = 0;
+
   for (size_t i = 0; i < amount; ++i)
   {
+    std::vector<double> matr_buf;
     size_t rw = 0, cl = 0;
     std::cin >> rw >> cl;
+    matr_buf.resize(rw * cl);
 
-    ch.push(MX::Matrix<chain::ldbl>{rw, cl});
+    for (size_t i = 0; i < rw * cl; ++i)
+    {
+      std::cin >> tmp_elem;
+      matr_buf[i] = tmp_elem;
+    }
+
+    ch.push(MX::Matrix<chain::ldbl>{rw, cl, matr_buf.begin(), matr_buf.end()});
+  
+    for (size_t i = 0; i < rw * cl; ++i)
+      std::cout << matr_buf[i] << " ";
+    
+    std::cout << std::endl;
+
+    matr_buf.clear();
   }
+
+  auto res = ch.multiply();
+
+  std::cout << res;
+
+  return 0;
 #endif
 
   chain::MatrixChain ch;
